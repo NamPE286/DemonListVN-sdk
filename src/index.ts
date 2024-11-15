e1df358f65400db16262bbd94c8653891e830d57
NamPE286 <nambuihung654@gmail.com>
2024-11-15 21:46:18 +0700

    Add EventManager class and integrate it into the Client class

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "./types/database.types";
 import LevelManager from "./classes/LevelManager";
 import PlayerManager from "./classes/PlayerManager";
+import EventManager from "./classes/EventManager";
 
 export class Client {
-    #db: SupabaseClient<Database>;
     #APIUrl: string;
+    db: SupabaseClient<Database>;
     level: LevelManager;
     player: PlayerManager;
+    event: EventManager;
 
     constructor(dbUrl: string, dbKey: string, APIUrl: string) {
-        this.#db = createClient<Database>(dbUrl, dbKey);
+        this.db = createClient<Database>(dbUrl, dbKey);
         this.#APIUrl = APIUrl;
-        this.level = new LevelManager(this.#db, this.#APIUrl);
-        this.player = new PlayerManager(this.#db, this.#APIUrl);
+        this.level = new LevelManager(this.db, this.#APIUrl);
+        this.player = new PlayerManager(this.db, this.#APIUrl);
+        this.event = new EventManager(this.db, this.#APIUrl)
     }
 }
